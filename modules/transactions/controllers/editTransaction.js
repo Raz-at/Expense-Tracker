49355e54174454516f374a436aa41ff0ee0b731e
Raz-at <razbaidya1@gmail.com>
2023-06-25 15:14:29 +0545

    This is the edit in edit transaction section

--- modules/transactions/controllers/editTransaction.js
+++ modules/transactions/controllers/editTransaction.js
@@ -9,16 +9,22 @@ const editTransaction = async (req, res) => {
   const { transaction_id, remarks, amount, transaction_type } = req.body;
   if (!transaction_id) throw "transaction_id is required";
 
-  if (!validator.isMongoId(transaction_id.toString()))
-    throw "Please provide a valid Id";
+  console.log(transaction_type);
 
-  if (transaction_type !== "income" || transaction_type !== "expense")
-    throw "transaction_type should only be income or expense";
+  if (!validator.isMongoId(transaction_id.toString()))
+    throw "Please provide a valid transaction Id";
+
+  // if (transaction_type !== "income" || transaction_type !== "expense")
+  //   throw "transaction_type should only be income or expense";
 
   const getTransaction = await transactionsModel.findOne({
     _id: transaction_id,
   });
+
   if (!getTransaction) throw "transaction not found";
+  console.log(getTransaction.amount);
+
+  console.log();
 
   await transactionsModel.updateOne(
     {
@@ -26,13 +32,105 @@ const editTransaction = async (req, res) => {
     },
     {
       remarks,
-      //   transaction_type,
-      //   amount,
+      transaction_type,
+      amount,
     },
     {
       runValidators: true,
     }
   );
+  console.log(getTransaction.amount);
+
+  if (
+    getTransaction.transaction_type === "income" &&
+    transaction_type == "income"
+  ) {
+    await userModel.updateOne(
+      {
+        _id: getTransaction.user_id,
+      },
+      {
+        $inc: { balance: getTransaction.amount * -1 }, //: getTransaction.amount,
+      },
+      {}
+    );
+    await userModel.updateOne(
+      {
+        _id: getTransaction.user_id,
+      },
+      {
+        $inc: { balance: amount },
+      },
+      {}
+    );
+  }
+  //
+  else if (
+    getTransaction.transaction_type === "income" &&
+    transaction_type == "expense"
+  ) {
+    await userModel.updateOne(
+      {
+        _id: getTransaction.user_id,
+      },
+      {
+        $inc: { balance: getTransaction.amount * -1 },
+      },
+      {}
+    );
+    await userModel.updateOne(
+      {
+        _id: getTransaction.user_id,
+      },
+      {
+        $inc: { balance: amount * -1 },
+      },
+      {}
+    );
+  }
+  //
+  else if (
+    getTransaction.transaction_type === "expense" &&
+    transaction_type == "expense"
+  ) {
+    await userModel.updateOne(
+      {
+        _id: getTransaction.user_id,
+      },
+      {
+        $inc: { balance: getTransaction.amount * 1 }, //: getTransaction.amount,
+      },
+      {}
+    );
+    await userModel.updateOne(
+      {
+        _id: getTransaction.user_id,
+      },
+      {
+        $inc: { balance: amount * -1 },
+      },
+      {}
+    );
+  } else {
+    await userModel.updateOne(
+      {
+        _id: getTransaction.user_id,
+      },
+      {
+        $inc: { balance: getTransaction.amount * 1 },
+      },
+      {}
+    );
+    await userModel.updateOne(
+      {
+        _id: getTransaction.user_id,
+      },
+      {
+        $inc: { balance: amount * 1 },
+      },
+      {}
+    );
+  }
 
   res.status(200).json({
     status: "Success",
